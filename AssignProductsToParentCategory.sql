c5c9a130b561440f26a98f79f5354c8f1ddf56d3
Scott Friend <scott.friend@vortx.com>
2015-04-10 15:20:41 -0700

    Deleted AssignProductsToParentCategory.sql

--- AssignProductsToParentCategory.sql
+++
@@ -1,8 +0,0 @@
-insert productcategory (categoryid, productid)
-
-select distinct c.parentcategoryid, productid from productcategory pc
-join category c on pc.categoryid = c.categoryid
-where convert(varchar(10), c.parentcategoryid) + '-' + convert(varchar(10), pc.productid) not in 
-(
-	select distinct convert(varchar(10), categoryid) + '-' + convert(varchar(10), productid) from productcategory
-)
